69f71839d71390dfbfe77dde9b294a99bcfe7322
Ильдар Д <115693673+Ildar-day@users.noreply.github.com>
2024-02-05 22:38:57 +0300

    Файлики

---
+++ Stepik.py
@@ -0,0 +1,21 @@
+print(1.2345e3)
+
+print(1.2345e-3)
+
+print(2014.0**14)
+
+print(7/3)
+
+print(int(-1.6))
+
+print(9**19 - int(float(9**19)))
+
+print(9**19)
+
+print(int(float(9**19)))
+
+print(type(2))
+
+X = int(input())
+Y = int(input())
+print(X*60 + Y)
